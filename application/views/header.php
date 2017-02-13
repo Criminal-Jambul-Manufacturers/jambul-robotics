59c0aa4e39c3fdd2e5947e9e7c24c486c3c527c5
ColtKing <king.colt@yahoo.ca>
2017-02-12 16:29:46 -0800

    Made the menu a little better

--- application/views/header.php
+++ application/views/header.php
@@ -1,6 +1,9 @@
-<ul class="list-inline">
+<ul class="list-inline" style="color: #431363;">
     <li><a href="/">Home</a></li>
+    <li>&bull;</li>
     <li><a href="/assembly">Assembly</a></li>
+    <li>&bull;</li>
     <li><a href="/parts">Parts</a></li>
+    <li>&bull;</li>
     <li><a href="/history">History</a></li>
 </ul>
