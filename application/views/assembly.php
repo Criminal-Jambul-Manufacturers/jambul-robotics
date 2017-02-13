c2768862488ec666f7345b8402a485114f6c8bc3
slamsquanch <zacmanshredz@gmail.com>
2017-02-12 17:03:13 -0800

    Assembly View and Controller updated

---
+++ application/views/assembly.php
@@ -0,0 +1,63 @@
+
+<div>
+    <h1>Assembly</h1>
+    <br/><br/><br/>
+</div>
+<div class="form-group">
+    <nav class="form-group">
+        <ul class="form-group">
+        <h3>Head</h3>
+            <form name="headForm" class="form-group">
+                <div align="center">
+                    <select name="headDropdown">
+                    {head}
+                         <option value="{partID}">ID: {partID}  Code: {partCode}</option>
+                    {/head}
+                    </select>
+                </div>
+            </form>
+            <h3>Torso</h3>
+            <form name="torsoForm" class="form-group">
+                <div align="center">
+                    <select name="torsoDropdown">
+                    {torso}
+                         <option value="{partID}">ID: {partID}  Code: {partCode}</option>
+                    {/torso}
+                    </select>
+                </div>
+            </form>
+            <h3>Legs</h3>
+            <form name="legsForm" class="form-group">
+                <div align="center">
+                    <select name="legsDropdown">
+                    {legs}
+                         <option value="{partID}">ID: {partID}  Code: {partCode}</option>
+                    {/legs}
+                    </select>
+                </div>
+            </form>
+        </ul>
+    </nav>
+    <br/> <br/>
+</div>
+<h2>Robots</h2>
+<div class="row">
+{robots}
+    <h3>Robot {robotID}</h3>
+    <div class="col-xs-3">
+            <a href="{headID}">
+            <img src="assets/img/parts/{head}.jpeg" width="50%" height="50%"/></br>
+        {head}</a>
+    </div>
+    <div class="col-xs-3">
+            <a href="{torsoID}">
+            <img src="assets/img/parts/{torso}.jpeg" width="50%" height="50%"/></br>
+        {torso}</a>
+    </div>
+    <div class="col-xs-3">
+            <a href="{bottomID}">
+            <img src="assets/img/parts/{bottom}.jpeg" width="50%" height="50%"/></br>
+        bottom}</a>
+    </div>
+{/robots}
+</div>
