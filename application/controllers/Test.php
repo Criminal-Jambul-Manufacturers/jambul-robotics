0e6d3a4ca98db1d9890a456cb425e2dab3343b06
Aaron Freytag <aaroncfreytag@gmail.com>
2017-03-31 16:43:50 -0700

    Added test page for testing purposes

---
+++ application/controllers/Test.php
@@ -0,0 +1,21 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends Application
+{
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('transaction');
+        $this->load->model('robot');
+        $this->load->model('part');
+        $this->load->library('pandaapi');
+    }
+    
+    public function index()
+    {
+        $this->data['pagebody'] = 'test';
+        $this->data['testmsg'] = $this->pandaapi->whoMakes("jambul");
+        $this->render(); 
+    }
+}
