1305cdbdc4e96876edfb9ccc330d1914159ee468
Aaron Freytag <aaroncfreytag@gmail.com>
2017-03-31 16:49:37 -0700

    Missed more of those pesky $s

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -15,7 +15,7 @@ class Test extends Application
     public function index()
     {
         $this->data['pagebody'] = 'test';
-        $this->data['testmsg'] = $this->pandaapi->whoMakes("jambul");
+        $this->data['testmsg'] = $this->pandaapi->getJob("jambul");
         $this->render(); 
     }
 }
