9bf6fb341e037dd9b71e594b1e001c85c170f5bf
slamsquanch <zacmanshredz@gmail.com>
2017-04-02 01:01:06 -0700

    manage page without the API at the moment.

--- application/controllers/Manage.php
+++ application/controllers/Manage.php
@@ -10,14 +10,63 @@ class Manage extends Application
 
     public function index()
     {
-        if($this->session->userdata('userrole') != ROLE_BOSS)
+        $role = $this->session->userdata('userrole');
+        if ($role != ROLE_BOSS)
         {
-            redirect($_SERVER['HTTP_REFERER']);
+            redirect($_SERVER['HTTP_REFERER']);// Go back to the page that we came from
         }
         else
         {
             $this->data['pagebody'] = 'manage';
+		    $this->data['pagetitle'] = 'Manage ('. $role . ')';
+            //$this->data['robot'] = $this->robot->all();
+
+            // build all the bots. 
+            $bots = array();
+            $botList = $this->robot->all();  //array of robot objects.
+            foreach($botList as $bot)
+            {
+                $imgHead =  $bot['model'].$bot['headID'].".jpeg";
+                $imgTorso = $bot['model'].$bot['torsoID'].".jpeg";
+                $imgBottom = $bot['model'].$bot['bottomID'].".jpeg";
+
+                $bots[] = array('BOT_ID'=>$bot['robotID'] , 'HEAD_ID'=>$bot['headID'], 'TORSO_ID' => $bot['torsoID'], 
+                    'BOTTOM_ID' => $bot['bottomID'], 'IMG_HEAD' => $imgHead, 'IMG_TORSO' => $imgTorso, 
+                    'IMG_BOTTOM' => $imgBottom);
+            }
+            
+            $this->data['robot'] = $robot;
+
+
             $this->render();
         }
     }
+
+    // Resets app.  Called by Manage View button.
+    function reboot() {
+
+    }
+
+
+    // Resets app. Uses the Manage View input box forms.
+    function register() {
+        $inputData = $this->input->post();
+		$team =   $this->input->post("name");  //plant name
+		$token =  $this->input->post("secret"); //token password.
+
+        //......  To be continued with API stuff.
+
+    }
+
+
+    //sell a robot. Called by Manage View button.
+    function sellRobot($botID) {
+
+    }
+
+
+
+
+
+
 }
--- application/views/manage.php
+++ application/views/manage.php
@@ -1,4 +1,56 @@
-<div>
-    <h2>Management</h2>
-    <br/>
+<h2>{pagetitle}</h2>
+<div class="row">
+    <div>
+        <form action="/Manage/register" method="post">
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label" for="registerName">Name Of Plant</label>
+                <div class="col-sm-10">
+                    <input type="text" name="registerName" class="form-control" placeholder="Jambul" >
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label" for="secret">Secret</label>
+                <div class="col-sm-10">
+                    <input type="password" name="secret" class="form-control" placeholder="nottelling" >
+                </div>
+            </div>    
+        </form>
+        <div class="col-sm-10">
+             <button type="submit" class="btn btn-primary">Register!</button>
+        </div>
+    </div>
+    <div>
+        <button class="btn btn-primary" onclick='reboot()'>REBOOT</button>
+    </div>
+    <div>
+        <table class="table-responsive">
+            <thead>
+                <tr>
+                    <th>Bot ID</th>
+                    <th>Build Info</th>
+                    <th>Appearance</th>
+                </tr> 
+            </thead>
+            <tbody>
+                {robot}
+                <tr>
+                    <td>
+                        {BOT_ID}
+                        Head: {HEAD_ID}
+                        Torso: {TORSO_ID}
+                        Bottom: {BOTTOM_ID}
+                    </td>
+                    <td>
+                        <img src="/assets/img/parts/{IMG_HEAD}" width="50%" height="50%"/></br>
+                        <img src="/assets/img/parts/{IMG_TORSO}" width="50%" height="50%"/></br>
+                        <img src="/assets/img/parts/{IMG_BOTTOM}" width="50%" height="50%"/></br>
+                    </td>
+                    <td>
+                        <button class='btn btn-sm' onclick='sellRobot("{BOT_ID}")'>Sell Me!</button>
+                    </td>
+                </tr>
+                {/robot}
+            </tbody>
+        </table>    
+    </div>
 </div>
